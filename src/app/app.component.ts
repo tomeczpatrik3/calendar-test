d1c6dd5bd35b8d1f80e2c9f9205b9d7e8eb10cef
Tomecz Patrik <>
2018-10-15 22:35:25 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,134 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import { CalendarEvent } from "angular-calendar";
+import {
+  isSameMonth,
+  isSameDay,
+  startOfMonth,
+  endOfMonth,
+  startOfWeek,
+  endOfWeek,
+  startOfDay,
+  endOfDay,
+  format
+} from "date-fns";
+import { Observable, of } from "rxjs";
+import { colors } from "./utils/colors";
+
+interface Film {
+  id: number;
+  title: string;
+  release_date: string;
+}
+
+const timezoneOffset = new Date().getTimezoneOffset();
+const hoursOffset = String(Math.floor(Math.abs(timezoneOffset / 60))).padStart(
+  2,
+  "0"
+);
+const minutesOffset = String(Math.abs(timezoneOffset % 60)).padEnd(2, "0");
+const direction = timezoneOffset > 0 ? "-" : "+";
+const timezoneOffsetString = `T00:00:00${direction}${hoursOffset}${minutesOffset}`;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'calendar-test';
+export class AppComponent implements OnInit {
+  view: string = "month";
+
+  viewDate: Date = new Date();
+
+  events$: Observable<Array<CalendarEvent<{ film: Film }>>>;
+
+  ownEvents$: Observable<Array<CalendarEvent<{ film: Film }>>>;
+
+  activeDayIsOpen: boolean = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchEvents();
+  }
+
+  fetchEvents(): void {
+    const getStart: any = {
+      month: startOfMonth,
+      week: startOfWeek,
+      day: startOfDay
+    }[this.view];
+
+    const getEnd: any = {
+      month: endOfMonth,
+      week: endOfWeek,
+      day: endOfDay
+    }[this.view];
+
+    const params = new HttpParams()
+      .set(
+        "primary_release_date.gte",
+        format(getStart(this.viewDate), "YYYY-MM-DD")
+      )
+      .set(
+        "primary_release_date.lte",
+        format(getEnd(this.viewDate), "YYYY-MM-DD")
+      )
+      .set("api_key", "0ec33936a68018857d727958dca1424f");
+
+    this.events$ = this.http
+      .get("https://api.themoviedb.org/3/discover/movie", { params })
+      .pipe(
+        map(({ results }: { results: Film[] }) => {
+          return results.map((film: Film) => {
+            return {
+              title: film.title,
+              start: new Date(film.release_date + timezoneOffsetString),
+              color: colors.yellow,
+              meta: {
+                film
+              }
+            };
+          });
+        })
+      );
+
+    this.events$.subscribe(res => {
+      console.log(res);
+    })
+
+    this.ownEvents$ = of([{
+      title: "TESZT",
+      start: new Date("2018-10-15"),
+      color: colors.yellow
+    }]);
+  }
+
+  dayClicked({
+    date,
+    events
+  }: {
+    date: Date;
+    events: Array<CalendarEvent<{ film: Film }>>;
+  }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+        this.viewDate = date;
+      }
+    }
+  }
+
+  eventClicked(event: CalendarEvent<{ film: Film }>): void {
+    window.open(
+      `https://www.themoviedb.org/movie/${event.meta.film.id}`,
+      "_blank"
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { AppComponent } from './app.component';
+import { CalendarHeaderComponent } from './components/calendar-head.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarHeaderComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    FormsModule,
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
+  ],
+  exports: [
+    CalendarHeaderComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
